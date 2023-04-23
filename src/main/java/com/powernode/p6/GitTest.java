2fcd5e066b110ffa4f41c7412d25d71cef88183c
Lkw <441469973@qq.com>
2023-04-23 14:37:21 +0800

    提交4

--- src/main/java/com/powernode/p6/GitTest.java
+++ src/main/java/com/powernode/p6/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("2222");
+        System.out.println("3333");
     }
 }
